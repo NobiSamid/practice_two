76eaede25d74044299b1df4b2a470370811950ed
NobiSamid <nobisamid@gmail.com>
2022-05-23 13:12:23 +0600

    api hit with axios then

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  state = { term: "" };
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) =>
+                this.setState({ term: e.target.value.toLocaleUpperCase() })
+              }
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
